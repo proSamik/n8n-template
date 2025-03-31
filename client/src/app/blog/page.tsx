86b159becee0124486a58e24829677e5518469d2
proSamik <samikchoudhury15@gmail.com>
2025-03-31 23:49:31 +0530

    Refactor BlogPage component to use server action for fetching blog posts. Updated the implementation to utilize fetchAllBlogPosts for improved data retrieval at build time, enhancing performance and maintainability.

--- client/src/app/blog/page.tsx
+++ client/src/app/blog/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import BlogPageClient from './BlogPageClient';
-import { getAllPosts } from '@/lib/blog-utils';
+import { fetchAllBlogPosts } from './actions';
 
 /**
  * Metadata for the blog page
@@ -19,9 +19,9 @@ export const metadata = {
  * Server component for the blog page
  * Fetches blog posts from markdown files in the public directory
  */
-export default function BlogPage() {
-  // Get all blog posts at build time
-  const posts = getAllPosts();
+export default async function BlogPage() {
+  // Get all blog posts at build time using the server action
+  const posts = await fetchAllBlogPosts();
   
   return <BlogPageClient posts={posts} />;
 } 
